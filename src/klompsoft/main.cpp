6b97c0a34395912e7bc407362e3c5255f9f24344
Rick Klomp <r.klomp@students.uu.nl>
2018-06-25 14:42:04 +0100

    Added a hook for script eval

---
+++ src/klompsoft/main.cpp
@@ -0,0 +1,17 @@
+#include <script/interpreter.h>
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+  cout << "Script validator boot.." << endl;
+//  bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, SigVersion sigversion, ScriptError* serror)
+
+
+  cout << "Script validator shutdown.." << endl;
+
+  return 0;
+}
