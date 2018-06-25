9cc6dac98b36a2988ab7e6f7390862c8d4922cc9
Rick Klomp <r.klomp@students.uu.nl>
2018-06-25 15:48:27 +0100

    Reading scripts from file..

--- src/klompsoft/main.cpp
+++ src/klompsoft/main.cpp
@@ -1,15 +1,44 @@
 #include <script/interpreter.h>
+#include "sig.h"
 
 #include <iostream>
+#include <fstream>
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-int main(void)
+int main(int argc, const char** argv)
 {
-  cout << "Script validator boot.." << endl;
-//  bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, SigVersion sigversion, ScriptError* serror)
+  if (argc < 2)
+    return 1;
 
+  std::ifstream input(argv[1], std::ios::binary );
+  // copies all data into buffer
+  std::vector<char> buffer((
+          std::istreambuf_iterator<char>(input)),
+          (std::istreambuf_iterator<char>()));
+
+  for(auto i : buffer)
+    cout << i << endl;
+  return 0;
+
+
+  std::vector<std::vector<unsigned char>> stack;
+  CScript script;
+  unsigned int flags = SCRIPT_VERIFY_NONE;
+  SigVersion sigVersion = SIGVERSION_BASE;
+  ScriptError err;
+  DisabledSignatureChecker sigchecker;
+
+  cout << "Script validator boot.." << endl;
+
+  //bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, unsigned int flags, const BaseSignatureChecker& checker, SigVersion sigversion, ScriptError* serror)
+
+  if (EvalScript(stack, script, flags, sigchecker, sigVersion, &err))
+    cout << "Eval: success" << endl;
+  else
+  {
+    cout << "Eval: failure, err: " << err << endl;
+  }
 
   cout << "Script validator shutdown.." << endl;
 
---
+++ src/klompsoft/sig.h
@@ -0,0 +1,22 @@
+#include <script/interpreter.h>
+
+class DisabledSignatureChecker : public BaseSignatureChecker
+{
+public:
+    virtual bool CheckSig(const std::vector<unsigned char>& scriptSig, const std::vector<unsigned char>& vchPubKey, const CScript& scriptCode, SigVersion sigversion) const
+    {
+        return true;
+    }
+
+    virtual bool CheckLockTime(const CScriptNum& nLockTime) const
+    {
+         return true;
+    }
+
+    virtual bool CheckSequence(const CScriptNum& nSequence) const
+    {
+         return true;
+    }
+
+    virtual ~DisabledSignatureChecker() {}
+};
